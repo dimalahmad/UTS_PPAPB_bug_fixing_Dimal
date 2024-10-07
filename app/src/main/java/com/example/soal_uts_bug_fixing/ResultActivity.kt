03cbf32d96a95c55d7567dbe6a0724f8ca6c9e6e
dimalahmad <dimalkarimahmad@mail.ugm.ac.id>
2024-10-07 11:42:41 +0700

    benerin yang kurang

--- app/src/main/java/com/example/soal_uts_bug_fixing/ResultActivity.kt
+++ app/src/main/java/com/example/soal_uts_bug_fixing/ResultActivity.kt
@@ -19,26 +19,30 @@ class ResultActivity : AppCompatActivity() {
 
         val tipe = intent.getStringExtra(AppointmentActivity.EXTRA_TIPE)
         val alamat = intent.getStringExtra(AppointmentActivity.EXTRA_ALAMAT)
+        val nama = intent.getStringExtra(FormActivity.EXTRA_NAMA)
+        val identitas = intent.getStringExtra(FormActivity.EXTRA_IDENTITAS)
+        val gender = intent.getStringExtra(FormActivity.EXTRA_GENDER)
         val tanggal = intent.getStringExtra(AppointmentActivity.EXTRA_TANGGAL)
         val waktu = intent.getStringExtra(AppointmentActivity.EXTRA_WAKTU)
 
 
         with(binding){
             namaTxt.text = nama
-            identitasTxt.text = identitas
             genderTxt.text = gender
+            identitasTxt.text = identitas
             tipeTxt.text = tipe
             if(tipe=="Offline"){
                 lokasiTitle.visibility = View.VISIBLE
                 lokasiTxt.visibility = View.VISIBLE
                 lokasiTxt.text = alamat
             }
+            tanggalTxt.text = tanggal
+            waktuTxt.text = waktu
 
             backBtn.setOnClickListener(){
                 val intentToHome = Intent(this@ResultActivity, MainActivity::class.java)
                 startActivity(intentToHome)
             }
         }
-
     }
 }
